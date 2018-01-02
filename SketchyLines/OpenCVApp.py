a9d301a1e2c34288b061b1b2292b332daed6a500
str4w <strawdog3@gmail.com>
2018-01-02 17:01:33 -0500

    Restructured - made application as a derived class from a base opencv application.  Have simple menu interaction, help menu, etc

---
+++ SketchyLines/OpenCVApp.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright 2018 Rupert Brooks
+
+Simple OpenCV app with onscreen overlay
+
+@author: rupert
+"""
+
+import cv2
+import numpy as np
+import time
+
+
+class OpenCVApp:
+    def __init__(self,windowTitle="Someone forgot to set a window title",videoDevice=0,delay=3):
+        self.windowTitle=windowTitle
+        self.videoDevice=videoDevice
+        self.delay=int(delay)
+        self.fps=0.
+        pass
+
+    def compute_fps(self):
+        now=time.time()
+        deltaTime=now-self.lastTime
+        self.frameTimesSum-=self.frameTimes[self.frameTimesIndex]
+        self.frameTimes[self.frameTimesIndex]=deltaTime
+        self.frameTimesSum+=self.frameTimes[self.frameTimesIndex]
+        self.frameTimesIndex=(self.frameTimesIndex+1)%len(self.frameTimes)
+        self.lastTime=now
+        self.fps=len(self.frameTimes)/self.frameTimesSum
+ 
+    def on_key(self,key):
+        if key != 255:
+            if key==27:
+                return False
+            else:
+                print("OpenCVApp: The key was",key)
+                
+        return True
+    
+    def process_frame(self,frame):
+        return frame
+
+    def overlay(self,frame):
+        resultFrame=frame.copy()
+        cv2.putText(resultFrame,"FPS: %3.1f"%(self.fps),
+                    (int(resultFrame.shape[0]*0.8), int(resultFrame.shape[1]*0.05)),
+                    cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,255))
+        return resultFrame
+
+    def run(self):
+        cv2.namedWindow(self.windowTitle)
+        capture = cv2.VideoCapture(self.videoDevice)
+        if capture is None or not capture.isOpened():
+            print ('OpenCVApp: Error: unable to open video source: %s'%(str(self.videoDevice)))
+            cv2.destroyWindow(self.windowTitle)
+            return
+        # sometimes cameras don't init right away, read until init.
+        startTime=time.time()
+        self.frameTimes=[0.]*10
+        self.frameTimesSum=0.
+        self.frameTimesIndex=0
+        ret = False
+        while(not ret):
+            ret,frame = capture.read()
+            if time.time()-startTime > 6.:
+                print ('OpenCVApp: Error: Unable to get a frame from the video source after 6 seconds, giving up')
+                return 
+
+        loop = True
+        self.lastTime=time.time()
+        while(loop):
+            ret,self.rawFrame = capture.read()
+            if (self.rawFrame is None):
+                break
+       
+            self.processedFrame=self.process_frame(self.rawFrame)
+            self.overlayFrame=self.overlay(self.processedFrame)
+            cv2.imshow(self.windowTitle,self.overlayFrame)
+       
+            char = cv2.waitKey(self.delay)
+            loop=self.on_key(char)
+            self.compute_fps()
+            
+        cv2.destroyWindow(self.windowTitle)
+
+    
+
+
+if __name__ == '__main__':
+    # simple test here.
+    App=OpenCVApp("Test OpenCVApp")
+    App.run()
--- SketchyLines/SketchyLines.py
+++ SketchyLines/SketchyLines.py
@@ -1,11 +1,14 @@
 """
 Sketchy Lines
-Copyright Rupert Brooks 2017
+Copyright Rupert Brooks 2017-2018
 """
 import cv2
 import numpy as np
+import os
 import sys
 import time
+
+from OpenCVApp import OpenCVApp
 
 def adjust_gamma(image, gamma=1.0):
 	# build a lookup table mapping the pixel values [0, 255] to
@@ -17,63 +20,113 @@ def adjust_gamma(image, gamma=1.0):
 	# apply gamma correction using the lookup table
 	return cv2.LUT(image, table)
 
+class SketchyLines(OpenCVApp):
+    def __init__(self,videoDevice=0,basename="frame_"):
+        super(SketchyLines,self).__init__("Sketchy Lines",videoDevice)
+        self.basename=basename
+        self.edgeCache=[]
+        self.maxEdges=10
+        self.outputDirectory="Output"
+        
+        self.showStats=False
+        self.showAppTitle=True
+        self.showHelpMenu=False
+        self.fontSizeSmall=0.4
+        self.fontThicknessSmall=1
+        self.fontThicknessLarge=2
+        self.fontSizeLarge=1.0
+        self.font=cv2.FONT_HERSHEY_SIMPLEX
+        self.statsColor=(255,0,0)
+        self.titleColor=(0,0,0)
+        self.menuColor=(255,255,255)
+        
+        
+    def save_frames(self):
+        if not os.path.exists(self.outputDirectory):
+            os.makedirs(self.outputDirectory)
+        timestamp=time.strftime("%Y%m%dT%H%M%S")
+        framename=self.outputDirectory+"/"+self.basename + timestamp +".png" 
+        print( "Saving "+framename)
+        cv2.imwrite(framename,self.processedFrame)
+        for i,e in enumerate(self.edgeCache):
+            framename="%s/%s%s_%02d.png"%(self.outputDirectory,self.basename,timestamp,i)
+            print( "Saving "+framename)
+            cv2.imwrite(framename,e)
 
-if __name__ == '__main__':
-    windowName="SketchyLines"
-    cv2.namedWindow(windowName)
-    videodevice=0
-
-    g_capture = cv2.VideoCapture(videodevice)
-    if g_capture is None or not g_capture.isOpened():
-        print ('Warning: unable to open video source: %s'%(str(videodevice)))
-    ret = False
-    while(not ret):
-       ret,frame = g_capture.read()
-
-    loop = True
-
-    basename="frame_"
-    if(len(sys.argv)>1):
-      basename=sys.argv[1]
-    edgecache=[]
-    maxedges=10
-    while(loop):
-       ret,frame = g_capture.read()
-       if (frame is None):
-          break
+    def on_key(self,key):
+        if   key == ord('f'): 
+            self.save_frames()
+        elif key == ord('h'): 
+            self.showHelpMenu=not self.showHelpMenu
+        elif key == ord('s'):     
+            self.showStats=not self.showStats
+        elif key == 27:
+            return False
+        
+        return True
+    
+    def process_frame(self,frame):
        for _ in range(1):
            frame=cv2.bilateralFilter(frame,d=7,sigmaColor=9,sigmaSpace=7)
-       edges = np.zeros_like(cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY))
-       cv2.Canny(frame,15000,3000,edges,7,False)
        gs=cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+       edges = np.zeros_like(gs)
+       cv2.Canny(frame,15000,3000,edges,7,False)
 
-       edgecache.append(edges)
-       if len(edgecache)>maxedges:
-           edgecache.pop(0)
-       edgeout=np.zeros_like(edges)
-       sumimage=np.zeros_like(edges,dtype=np.float64)
-       for e in edgecache:
-           sumimage+=e
-       edgeout=np.uint8(np.minimum(2.0*sumimage/len(edgecache),255))
+       self.edgeCache.append(edges)
+       if len(self.edgeCache)>self.maxEdges:
+           self.edgeCache.pop(0)
+       self.sumimage=np.zeros_like(edges,dtype=np.float64)
+       for e in self.edgeCache:
+           self.sumimage+=e
+       self.edgeout=np.uint8(np.minimum(2.0*self.sumimage/len(self.edgeCache),255))
 #       print (frame.shape)
        
-       outimg=np.minimum(cv2.cvtColor((255-edgeout),cv2.COLOR_GRAY2RGB),adjust_gamma(frame,2.0))
-#       outimg=cv2.cvtColor((255-edgeout),cv2.COLOR_GRAY2RGB)
-#       print(outimg.shape)
-       cv2.imshow(windowName,outimg)
-       
-       char = cv2.waitKey(3)
-       if (char != -1):
-          if(char == 102):
-             timestamp=time.strftime("%Y%m%dT%H%M%S")
-             framename=basename + timestamp +".png" 
-             print( "Saving "+framename)
-             cv2.imwrite(framename,outimg)
-             for i,e in enumerate(edgecache):
-                 framename="%s%s_%d.png"%(basename,timestamp,i)
-                 print( "Saving "+framename)
-                 cv2.imwrite(framename,e)
-          elif (char == 27):
-             loop = False
-    cv2.destroyWindow(windowName)
+       outimg=np.minimum(cv2.cvtColor((255-self.edgeout),cv2.COLOR_GRAY2RGB),adjust_gamma(frame,2.0))
+       return outimg
+    
+    def overlay(self,frame):
+        resultFrame=frame.copy()
+        if self.showStats:
+            fpsText="FPS: %3.1f"%(self.fps)
+            szfps,_=cv2.getTextSize(fpsText,self.font,self.fontSizeSmall,self.fontThicknessSmall)
+            edgeText="Edge Px: %d"%(np.sum(self.sumimage))
+            szedge,_=cv2.getTextSize(edgeText,self.font,self.fontSizeSmall,self.fontThicknessSmall)
+            posX=resultFrame.shape[1]-max([szfps[0],szedge[0]])-10
+            cv2.putText(resultFrame,fpsText,
+                        (posX, int(resultFrame.shape[0]*0.85)),
+                        self.font,self.fontSizeSmall,self.statsColor,self.fontThicknessSmall)
+            cv2.putText(resultFrame,edgeText,
+                        (posX, int(resultFrame.shape[0]*0.9)),
+                        self.font,self.fontSizeSmall,self.statsColor,self.fontThicknessSmall)
+        if self.showAppTitle:
+            sz,_=cv2.getTextSize("Sketchy Lines",self.font,self.fontSizeLarge,self.fontThicknessLarge)
+            cv2.putText(resultFrame,"Sketchy Lines",
+                        (int(resultFrame.shape[1]*0.5-sz[0]*0.5), int(resultFrame.shape[0]*0.05)),
+                        self.font,self.fontSizeLarge,self.titleColor,self.fontThicknessLarge)
+            sz,_=cv2.getTextSize("Press h for help",self.font,self.fontSizeSmall,self.fontThicknessSmall)
+            cv2.putText(resultFrame,"Press h for help",
+                        (int(resultFrame.shape[1]*0.5-sz[0]*0.5), int(resultFrame.shape[0]*0.1)),
+                        self.font,self.fontSizeSmall,self.titleColor,self.fontThicknessSmall)
+        if self.showHelpMenu:
+            cv2.putText(resultFrame,"h - show / hide this menu",
+                        (int(resultFrame.shape[1]*0.1), int(resultFrame.shape[0]*0.2)),
+                        self.font,self.fontSizeSmall,self.menuColor,self.fontThicknessSmall)
+            cv2.putText(resultFrame,"f - save current set of frames",
+                        (int(resultFrame.shape[1]*0.1), int(resultFrame.shape[0]*0.25)),
+                        self.font,self.fontSizeSmall,self.menuColor,self.fontThicknessSmall)
+            cv2.putText(resultFrame,"s - show / hide stats",
+                        (int(resultFrame.shape[1]*0.1), int(resultFrame.shape[0]*0.3)),
+                        self.font,self.fontSizeSmall,self.menuColor,self.fontThicknessSmall)
+            cv2.putText(resultFrame,"ESC - quit",
+                        (int(resultFrame.shape[1]*0.1), int(resultFrame.shape[0]*0.35)),
+                        self.font,self.fontSizeSmall,self.menuColor,self.fontThicknessSmall)
+        return resultFrame
+
+
+
+if __name__ == '__main__':
+    
+    App=SketchyLines()
+    App.run()
+    
 
